0a27a3e11ebe488e6ac431b7c565316620d5aec7
Bilal Rukundi <bilalrukundi1658@gmail.com>
2026-07-14 21:42:17 +0530

    the new start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Start the Celery worker and beat schedule in the background (&)
+celery -A app.worker.celery_app worker --beat -l info -Q fb-queue,ig-queue,li-queue &
+
+# Start the FastAPI web server in the foreground
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
